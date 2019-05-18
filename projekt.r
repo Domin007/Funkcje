b776d38178cec3fa03d4a643de532b0f1aade098
bartekmj <48033473+bartekmj@users.noreply.github.com>
2019-05-18 19:35:25 +0200

    Update projekt.r

--- projekt.r
+++ projekt.r
@@ -1,1 +1,46 @@
+#1. Konwersja Celsjusz na Farenheit
+
+konwersja_c_f = function(temperatura_c){
+  (temperatura_c *1.8) + 32
+}
+konwersja_c_f(20) 
+  
+#2. Konwersja Celsjusz na Kelvin
+
+konwersja_c_k = function(temperatura_c){
+  (temperatura_c +273.15)
+}
+konwersja_c_k(50) 
+
+
+#3. Konwersja Fahrenheit na Kelvin
+
+konwersja_f_k = function(temperatura_f){
+  (temperatura_f + 459.67) *5/9
+}
+konwersja_f_k(30)
+
+#4. Konwersja Kelvin na Celsjusz
+
+konwersja_k_c = function(temperatura_k){
+  (temperatura_k - 273.15)
+}
+konwersja_k_c(45)
+
+#5. Konwersja Celsjusz na Rankine
+konwersja_c_ra = function(temperatura_c){
+  (temperatura_c + 273.15) * 1.8
+}  
+konwersja_c_ra(20)  
+
+#6. Konwersja Celsjusz na Reaumur
+konwersja_c_re = function(celsj){
+  (temp_r=celsj*4/5)
+}
+konwersja_c_re(100)
+
+
+
+
+
 
