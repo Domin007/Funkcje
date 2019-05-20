89aaf3c0c1c0ed554de27903b167b0a9ca664e9a
bartekmj <48033473+bartekmj@users.noreply.github.com>
2019-05-20 15:19:45 +0200

    Update projekt.r

--- projekt.r
+++ projekt.r
@@ -41,6 +41,53 @@ konwersja_c_re(100)
 
 
 
+#7. Waluta zl na eur
+waluta_zl_eur = function(zl){
+  (kurs=zl/4.307)
+}
+waluta_zl_eur(50)
 
+#8. Waluta eur na zl
+waluta_eur_zl = function(zl){
+  (kurs=zl*4.307)
+}
+waluta_eur_zl(50)
+
+#9. Waluta zl na usd
+waluta_zl_usd = function(zl){
+  (kurs=zl/3.854)
+}
+waluta_zl_usd(20)
+
+#10. Waluta usd na zl
+waluta_usd_zl = function(zl){
+  (kurs=zl*3.854)
+}
+waluta_usd_zl(20)
+
+#11. Waluta zl na gbp
+waluta_zl_gbp = function(zl){
+  (kurs=zl/4.927)
+}
+waluta_zl_gbp(100)
+
+#12. Waluta gbp na zl
+waluta_gbp_zl = function(zl){
+  (kurs=zl*4.927)
+}
+waluta_gbp_zl(100)
+
+
+#13. waluta zl na rub
+waluta_zl_rub = function(zl){
+  (kurs=zl/0.0597)
+}
+waluta_zl_rub(50)
+
+#14. waluta rub na zl
+waluta_rub_zl = function(zl){
+  (kurs=zl*0.0597)
+}
+waluta_rub_zl(50)
 
 
