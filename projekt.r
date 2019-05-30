7ac6990eb6ddaef09a4a98b398606cf82f4299dd
bartekmj <48033473+bartekmj@users.noreply.github.com>
2019-05-30 21:36:56 +0200

    Update projekt.r

--- projekt.r
+++ projekt.r
@@ -1,63 +1,89 @@
-#' Konwersja temperatur I
-#' 
-#' @description Funkcja sluzaca do konwersji
-#'  temperatury ze stopni Celsjusza do stopni Fahrenheita 
+#' Konwersja temperatur I --------------------------------------------------
+#'  
+#' @description Funkcja sluzaca do konwersji temperatury ze
+#'  stopni Celsjusza do stopni Fahrenheita i na odwrot
 #' 
 #' @comment Skala Fahrenheita uzywana jest m.in. w USA 
 #'  zero przesuniete jest wzgledem zera skali Celsjusza o wartosc 32
 #' 
 #' @param temperatura_c wektor zawierajacy wartosci temperatury
 #'  w stopniach Celsjusza
+#' @param temperatura_f wektor zawierajacy wartosci temperatury
+#'  w stopniach Fahrenheita
 #' 
-#' @return wektor numeryczny odpowiadajacy skali Fahrenheita
+#' @return wektor numeryczny odpowiadajacy 
+#'  skali Fahrenheita lub Celsjusza
 #' 
 #' @examples
 #' konwersja_c_f(20) 
-#' konwersja_c_f(-65) 
 #' konwersja_c_f(36.6)
 #' konwersja_c_f(c(10,30,82)) 
+#' 
+#' konwersja_f_c(20)
+#' konwersja_f_c(50)
+#' konwersja_f_c(c(10,30,82)) 
 
 
 konwersja_c_f = function(temperatura_c){
   (temperatura_c *1.8) + 32
 }
 
+konwersja_f_c = function(temperatura_f){
+  (temperatura_f - 32) / 1.8
+}
 
-#' Konwersja temperatur II
+
+
+#' Konwersja temperatur II -------------------------------------------------
+#' 
 #' 
 #' @description Funkcja sluzaca do konwersji temperatury 
-#'  ze stopni Fahrenheita do stopni Kelvina
+#'  ze stopni Fahrenheita do stopni Kelvina i na odwrot
 #' 
 #' @comment Skala Kelvina nazywana jest bezwzgledna skala temperatur,
 #'  powszechnie stosowana w badaniach naukowych na calym swiecie.
 #'  
 #' @param temperatura_f wektor zawierajacy wartosci temperatury
 #'  w stopniach Fahrenheita
+#' @param temperatura k wektor zawierajacy wartosci temperatury
+#'  w stopnaich Kelvina
 #'  
 #' @return wektor numeryczny odpowiadajacy skali Kelvina
 #'
 #' @examples
 #' konwersja_f_k(10)
-#' konwersja_f_k(111)
 #' konwersja_f_k(0)
-#' konwersja_f_k(c(15,115,1115))
+#' konwersja_f_k(-500)
+#' 
+#' konwersja_k_f(50)
+#' konwersja_k_f(-1)
+#' konwersja_k_f(25)
 
 
 konwersja_f_k = function(temperatura_f){
-  (temperatura_f + 459.67) *5/9
+  (temperatura_f + 459.67) *5/9 || (temperatura_f < -459.67)
+    stop("temperatura w stopniach Kelvina nie moze byc ujemna")
 }
 
------------------------------------------------------------------------------------------
+konwersja_k_f = function(temperatura_k){
+  (temperatura_k *1.8 - 459.67) || (temperatura_k < 0)
+  stop("temperatura w stopniach Kelvina nie moze byc ujemna")
+}
 
-#'Konwersja walut I
-#'
+
+
+#' Konwersja walut I -------------------------------------------------------
+#' 
+#' 
 #' @description Funkcja sluzaca zmianie waluty PLN
 #'  na walute EUR i na odwrot
+#'  kurs z dnia: 22-05-2019
 #' 
 #' @comment Przy zamianie walut nalezy wziasc pod uwage kurs,
 #'  ktory z dnia na dzien zmienia swoja wartosc
 #'  
 #' @param zl wektor odpowiadajacy wartosci w walucie PLN
+#' @param eur wektor odpowiadajacy wartosci w walucie EUR
 #' 
 #' @return wektor numeryczny
 #' 
@@ -70,7 +96,7 @@ konwersja_f_k = function(temperatura_f){
 #' waluta_eur_zl(500) 
 #' waluta_eur_zl(c(10,40,90))
 
- 
+
 waluta_zl_eur = function(zl){
   (kurs=zl/4.307)
 }
@@ -80,16 +106,18 @@ waluta_eur_zl = function(zl){
 }
 
 
-
-#' Konwersja walut II
+#' Konwersja walut II ------------------------------------------------------
+#' 
 #' 
 #' @description Funkcja sluzaca zmianie waluty PLN
 #'  na walute USD i na odwrot 
+#'  kurs z dnia: 22-05-2019
 #' 
 #' @comment Dolar amerykanski jest najwazniejsza waluta swiata,
 #'  rozlicza sie w nim wiekszosc panstw swiata
 #'  
 #' @param zl wektor odpowiadajacy wartosci pieniedzy w walucie PLN
+#' @param USD wektor odpowiadajacy wartosci pieniedzy w walucie dolara amerykanskiego
 #' 
 #' @return wektor numeryczny
 #' 
@@ -115,9 +143,8 @@ waluta_usd_zl = function(zl){
 waluta_usd_zl(20)
 
 
------------------------------------------------------------------------------------------
-  
-#' Konwersja predkosci I  
+#' Konwersja predkosci I ---------------------------------------------------
+#' 
 #' 
 #' @description Funkcja sluzaca zamianie km/h na mph i na odwrot
 #' 
@@ -129,11 +156,7 @@ waluta_usd_zl(20)
 #' 
 #' @return wektor numeryczny
 #' 
-#' @warning
-predkosc = -5
-if(predkosc < 0){
-  cat("predkosc nie moze byc ujemna")
-}
+#' @warning predkosc nie moze byc ujemna
 #' 
 #' @examples 
 #' zamiana_kmh_mph(50)
@@ -143,10 +166,10 @@ if(predkosc < 0){
 #' zamiana_mph_kmh(50)
 #' zamiana_mph_kmh(140)
 #' zamiana_mph_kmh(c(30,100,150))
-  
+
 
 zamiana_kmh_mph = function(km){
-  (predkosc=km/1.609344)
+  (predkosc=km/1.609344) 
 }
 
 zamiana_mph_kmh = function(km){
@@ -155,7 +178,8 @@ zamiana_mph_kmh = function(km){
 
 
 
-#' Konwersja predkosci II
+#' Konwersja predkosci II --------------------------------------------------
+#' 
 #' 
 #' @description Funkcja sluzaca zamianie km/h na m/s i na odwrot
 #' 
@@ -166,11 +190,7 @@ zamiana_mph_kmh = function(km){
 #' 
 #' @return wektor numeryczny
 #' 
-#' @warning
-predkosc = - 1
-if(predkosc < 0){
-  cat("predkosc nie moze byc ujemna")
-}
+#' @warning predkosc nie moze byc ujemna
 #' 
 #' @examples 
 #' zamiana_kmh_ms(50)
@@ -192,27 +212,22 @@ zamiana_ms_kmh = function(km){
 }
 
 
------------------------------------------------------------------------------------
 
 
-# Konwersja odleglosci morskich I
-
+#' Konwersja odleglosci morskich -------------------------------------------
+#' 
+#' 
 #' @description Funkcja sluzaca do konwersji odleglosci z km na miedzynarodowe mile morskie(inm) i na odwrot  
 #' 
 #' @comment Umiejetnosc przeliczania tych jednostek wykorzystywana jest w nawigacji morskiej oraz w lotnictwie,
 #' dotyczy dlugosci, glebokosci, predkosci i kierunku
 #'
-#' 
 #' @param km wektor zawierajacy wartosci odleglosci w kilometrach
 #' 
 #' @return wektor numeryczny
 #' 
-#' @warning 
-odleglosc = -2 
-if(odleglosc < -2){
-  cat("odleglosc nie moze byc ujemna")
-}
-
+#' @warning odleglosc nie moze byc ujemna
+#' 
 #' @examples
 #' zamiana_km_inm(20)
 #' zamiana_km_inm(100)
@@ -221,10 +236,10 @@ if(odleglosc < -2){
 #' zamiana_inm_km(20)
 #' zamiana_inm_km(100)
 #' zamiana_inm_km(c(200,350,999))
- 
-   
+
+
 zamiana_km_inm = function(km){ 
- (odleglosc = km / 1.852)
+  (odleglosc = km / 1.852)
 } 
 
 zamiana_inm_km = function(km){
@@ -232,15 +247,17 @@ zamiana_inm_km = function(km){
 }
 
 
-  
-# Konwersja dlugosci
 
-#' @description Funkcja sluzaca do zamiany jednostek
-#'  dlugosci z cali na centrymetry
+#' Konwersja dlugosci ------------------------------------------------------
 #' 
+#' 
+#' @description Funkcja sluzaca do zamiany jednostek
+#'  dlugosci z cali na centrymetry i na odwrot
+#'  
 #' @param cal wektor odpowiadajacy dlugosci w cal
+#' @param cm wektor odpowiadajacy dlugosci w cm
 #'
-#' @return wektor numeryczny w cm
+#' @return wektor numeryczny w cm lub cal
 #'
 #' @warning dlugosci ujemne nie istnieja
 #'
@@ -248,12 +265,18 @@ zamiana_inm_km = function(km){
 #' zamiana_cale_cm(2)
 #' zamiana_cale_cm(0)
 #' zamiana_cale_cm(c(10,20,30))
-
+#' 
+#' zamiana_cm_cale(5)
+#' zamiana_cm_cale(50)
+#' zamiana_cm_cale(c(10,30,60))
 
 zamiana_cale_cm = function(cm){
   (cal = 2.54*cm)
 }
 
+zamiana_cm_cale = function(cm){
+  (cal = 0.39370*cm)
+}
 
 
 
